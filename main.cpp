af96ebca0dc41102ead67b45d9a1ff8f863eec93
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2025-12-10 20:06:16 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+void ordina(int n, int v[]) 
+{
+    int i, j, t;
+    for (i = 0; i <= n - 1; i++) 
+    {
+        for (j = 0; j <= n - 2; j++) 
+        {
+            if (v[j] > v[j + 1]) 
+            {
+                t = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = t;
+            }
+        }
+    }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+void visualizza(int n, int v[]) 
+{
+    int i;
+    for (i = 0; i <= n - 1; i++) 
+    {
+        cout << v[i] << endl;
+    }
+}
+
+int main() 
+{
+    int n, i;
+    n = 3;
+    int v[n], w[n];
+    i = 0;
+    while (i <= n - 1) 
+    {
+        v[i] = rand() % (n * 10 + 1);
+        w[i] = rand() % (n * 10 + 1);
+        i = i + 1;
+    }
+    ordina(n, v);
+    ordina(n, w);
+    cout << "vettore V ordinato" << endl;
+    visualizza(n, v);
+    cout << "vettore W ordinato" << endl;
+    visualizza(n, w);
+    return 0;
+}
